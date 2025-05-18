4784de90f7f6d4027bb3a015d9c18f601a654de0
caaeen <canadatofer@gmail.com>
2025-05-18 16:15:16 +0800

    hjfd

--- server/db.js
+++ server/db.js
@@ -3,11 +3,11 @@ require("dotenv").config(); //required when using .env file
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
 });
 
 
